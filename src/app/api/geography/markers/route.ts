655466bd46eaaf469d326c67427487f16fed54ec
Aleksandr Zelenskiy <al.a.zelenskiy@gmail.com>
2026-02-02 16:28:35 +0800

    Fix build app error

--- src/app/api/geography/markers/route.ts
+++ src/app/api/geography/markers/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 
-import { getRegionLabelByCode } from "@/lib/regions";
+import { getRegionLabelByCode } from "../../../../lib/regions";
 
 type RegionStat = {
   regionCode: string;
